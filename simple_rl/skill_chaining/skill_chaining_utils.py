5bedb72f0c8f3e529259c207259b27ef20898ea5
Akhil Bagaria <akhil_bagaria@brown.edu>
2018-10-29 15:19:19 -0400

    Added a function to visualize the learned policy of an option

--- simple_rl/skill_chaining/skill_chaining_utils.py
+++ simple_rl/skill_chaining/skill_chaining_utils.py
@@ -1,6 +1,10 @@
 # Python imports.
+import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
+
+# Other imports.
+from simple_rl.abstraction.action_abs.OptionClass import Experience
 
 def plot_trajectory(trajectory, show=True, color='k'):
     """
@@ -31,4 +35,19 @@ def plot_all_trajectories_in_initiation_data(initiation_data):
 
 def plot_initiation_set(option):
     X0, X1 = np.array(option.initiation_examples)[:, 0], np.array(option.initiation_examples)[:, 1]
+    pass
+
+def visualize_option_policy(option):
+    colors = ("red", "green", "blue", "yellow")
+    # option.experience_buffer is a matrix with 10 columns representing the 10 times the option's
+    # goal was encountered. Reshape as a column vector of Experience objects
+    experience_buffer = option.experience_buffer.reshape(-1)
+    x_positions = [experience.state.x for experience in experience_buffer]
+    y_positions = [experience.state.y for experience in experience_buffer]
+    actions = [option.solver.act(experience.state.features()) for experience in experience_buffer]
+    color_map = [colors[action] for action in actions]
+    plt.scatter(x_positions, y_positions, c=color_map, alpha=0.7, edgecolors='none')
+    plt.xlabel("x"); plt.ylabel("y"); plt.title("{} policy \nred: noop, green: left, blue: right, yellow: main".format(option.name))
+    plt.show()
+    return x_positions, y_positions, actions
 
