f5ee6b70103e65408c695c7d57e70a2c4fe6b367
Akhil Bagaria <akhil_bagaria@brown.edu>
2018-11-03 18:31:13 -0400

    Added file to compare skill chaining and dqn over multiple runs

---
+++ simple_rl/skill_chaining/experiments.py
@@ -0,0 +1,59 @@
+# Python imports.
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+sns.set()
+
+# Other imports.
+from simple_rl.skill_chaining.SkillChainingClass import SkillChaining, construct_lunar_lander_mdp
+from simple_rl.agents.func_approx.TorchDQNAgentClass import DQNAgent, main
+
+class SkillChainingExperiments(object):
+    def __init__(self, mdp, num_episodes=2000, num_instances=5, random_seed=0):
+        self.mdp = mdp
+        self.num_episodes = num_episodes
+        self.num_instances = num_instances
+        self.random_seed = random_seed
+
+    def compare_agents(self):
+        print("=" * 80)
+        print("Training skill chaining agent..")
+        print("=" * 80)
+        list_scores, episode_numbers = [], []
+        for i in range(self.num_instances):
+            print("\nInstance {} of {}".format(i+1, self.num_instances))
+            solver = DQNAgent(self.mdp.env.observation_space.shape[0], self.mdp.env.action_space.n, 0)
+            skill_chaining_agent = SkillChaining(self.mdp, self.mdp.goal_predicate, rl_agent=solver)
+            episodic_scores = skill_chaining_agent.skill_chaining(num_episodes=self.num_episodes)
+            episode_numbers += list(range(self.num_episodes))
+            list_scores += episodic_scores
+
+        print("\n")
+        print("=" * 80)
+        print("Training baseline DQN agent..")
+        print("=" * 80)
+
+        dqn_list_scores = []
+        for i in range(self.num_instances):
+            print("\nInstance {} of {}".format(i+1, self.num_instances))
+            dqn_scores = main(num_training_episodes=self.num_episodes)
+            dqn_list_scores += dqn_scores
+
+        scores = list_scores + dqn_list_scores
+        episodes = episode_numbers + episode_numbers
+        algorithm = ["skill_chaining"] * len(episode_numbers) + ["dqn"] * len(episode_numbers)
+        scores_dataframe = pd.DataFrame(np.array(scores), columns=["reward"])
+        scores_dataframe["episode"] = np.array(episodes)
+
+        plt.figure()
+        scores_dataframe["method"] = np.array(algorithm)
+        sns.lineplot(x="episode", y="reward", hue="method", data=scores_dataframe)
+        plt.savefig("skill_chaining_comparison.png")
+
+if __name__ == '__main__':
+    overall_mdp = construct_lunar_lander_mdp()
+    experiments = SkillChainingExperiments(overall_mdp)
+    experiments.compare_agents()
