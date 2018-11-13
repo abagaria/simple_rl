dace737dd8667e471bbd456160a7c1d592533946
Akhil Bagaria <akhil_bagaria@brown.edu>
2018-11-13 15:28:04 -0500

    DQN: Increase replay buffer size to a million so that we can avoid the forgetting problem after a while

--- simple_rl/agents/func_approx/TorchDQNAgentClass.py
+++ simple_rl/agents/func_approx/TorchDQNAgentClass.py
@@ -14,7 +14,7 @@ import torch.nn.functional as F
 from simple_rl.agents.AgentClass import Agent
 
 ## Hyperparameters
-BUFFER_SIZE = int(1e5)  # replay buffer size
+BUFFER_SIZE = int(1e6)  # replay buffer size
 BATCH_SIZE = 64  # minibatch size
 GAMMA = 0.99  # discount factor
 TAU = 1e-3  # for soft update of target parameters
